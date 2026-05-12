0ff9ce9f2331ef7833db5dee10dcf46f91f80043
sabrinakayla <sabrinakuliah10@gmail.com>
2026-05-12 18:31:00 +0700

    implementasi fungsi void pasien & dokter

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -31,3 +31,28 @@ public:
     void tambahPasien(pasien*);
     void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) { // auto digunakan dalam perulangan for untuk secara otomatis menentukan tipe data dari elemen yang diitera
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+
+void dokter::cetakPasien() {
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
