5b37026fe6de87d7dee22a1dbbb657c9502ede93
sabrinakayla <sabrinakuliah10@gmail.com>
2026-05-12 18:00:28 +0700

    membuat class ibu

--- ibu.h
+++ ibu.h
@@ -1,3 +1,19 @@
 #ifndef IBU_H
 #define IBU_H
 #include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
