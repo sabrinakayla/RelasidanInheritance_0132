59256ffa31f9e109c3c4deeaba974b6174c61ee7
sabrinakayla <sabrinakuliah10@gmail.com>
2026-05-12 18:40:25 +0700

    implementasi fungsi class orang & class pelajar

--- inheritance.cpp
+++ inheritance.cpp
@@ -1,3 +1,32 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class orang {
+public:
+    string nama;
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+};
+
+class pelajar : public orang {
+public:
+    string sekolah;
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+    }
+};
